1e62c7c6d6df7f4d62afe94bfaae82a803e0e22e
Milton Bolonha <miltonsiedler@gmail.com>
2026-06-17 07:31:28 -0300

    bugfix/build estampas

--- src/modules/io-estampas/containers/EstampasApp.tsx
+++ src/modules/io-estampas/containers/EstampasApp.tsx
@@ -3,19 +3,27 @@ import { HomeStorePicker } from '../components/HomeStorePicker';
 import { EditorContainer } from './EditorContainer';
 import { useCurrentWorkspace } from '@/lib/stores';
 
-export default function EstampasApp() {
+interface Props {
+  storeName?: string;
+  isInsideAdmin?: boolean;
+  onExit?: () => void;
+}
+
+export default function EstampasApp({ storeName: externalStoreName, isInsideAdmin, onExit }: Props) {
   const currentWorkspace = useCurrentWorkspace();
   const [storeName, setStoreName] = useState<string | null>(null);
 
   useEffect(() => {
-    if (currentWorkspace?.name) {
+    if (externalStoreName) {
+      setStoreName(externalStoreName);
+    } else if (currentWorkspace?.name) {
       setStoreName(currentWorkspace.name);
     }
-  }, [currentWorkspace]);
+  }, [currentWorkspace, externalStoreName]);
 
   if (!storeName) {
     return <HomeStorePicker onStart={setStoreName} />;
   }
 
-  return <EditorContainer storeName={storeName} onExit={() => setStoreName(null)} isInsideAdmin={!!currentWorkspace} />;
+  return <EditorContainer storeName={storeName} onExit={() => { setStoreName(null); if (onExit) onExit(); }} isInsideAdmin={isInsideAdmin ?? !!currentWorkspace} />;
 }
